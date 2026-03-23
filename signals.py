8d18da79e965ffe03ca7665fce880850e47e8359
diego villalba <diegov101418@gmail.com>
2026-03-23 17:18:39 -0300

    s

---
+++ signals.py
@@ -0,0 +1,14 @@
+def generate_signal():
+
+    # ejemplo simple (vos enchufás tu scanner + IA)
+    signals = []
+
+    # simulación
+    signals.append({
+        "symbol": "BTCUSDT",
+        "side": "BUY",
+        "confidence": 0.8,
+        "price": 50000
+    })
+
+    return signals
